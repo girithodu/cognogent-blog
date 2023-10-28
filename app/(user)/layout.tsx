fc8722c8807b889cf948838b4c0be5822626403a
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-10-28 12:37:21 -0700

    Seperate layouts for user and admin pages

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <div className='p-8 bg-orange-300'>Am I everywhere</div> */}
       <body className={inter.className}>{children}</body>
     </html>
   )
